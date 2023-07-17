5563ea5c8eb4133867d409d027d25b12a579cf8f
FelipeBragaUEA <83717754+FelipeBragaUEA@users.noreply.github.com>
2023-07-17 01:47:00 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request
 import datetime
 import hashlib
-import random
 
 app = Flask(__name__)
 
 class Block:
-    def __init__(self, index, timestamp, data, previous_hash, validator):
+    def __init__(self, index, timestamp, data, previous_hash):
         self.index = index
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
-        self.validator = validator
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        data = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.validator)
+        data = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
         return hashlib.sha256(data.encode()).hexdigest()
 
 
@@ -24,7 +22,7 @@ class Blockchain:
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        return Block(0, datetime.datetime.now(), "Genesis Block", "0", None)
+        return Block(0, datetime.datetime.now(), "Genesis Block", "0")
 
     def get_latest_block(self):
         return self.chain[-1]
@@ -48,33 +46,7 @@ class Blockchain:
         return True
 
 
-class ProofOfStake:
-    def __init__(self, validators):
-        self.validators = validators
-
-    def select_validator(self):
-        total_stake = sum([validator['stake'] for validator in self.validators])
-        random_number = random.uniform(0, total_stake)
-        cumulative_stake = 0
-
-        for validator in self.validators:
-            cumulative_stake += validator['stake']
-            if cumulative_stake >= random_number:
-                return validator
-
-        return None
-
-
 blockchain = Blockchain()
-
-# Sample validators with stake amounts
-validators = [
-    {'name': 'Validator 1', 'stake': 10},
-    {'name': 'Validator 2', 'stake': 5},
-    {'name': 'Validator 3', 'stake': 3},
-]
-
-proof_of_stake = ProofOfStake(validators)
 
 @app.route('/')
 def index():
@@ -83,8 +55,7 @@ def index():
 @app.route('/add_record', methods=['POST'])
 def add_record():
     data = request.form['data']
-    selected_validator = proof_of_stake.select_validator()
-    new_block = Block(len(blockchain.chain), datetime.datetime.now(), data, "", selected_validator)
+    new_block = Block(len(blockchain.chain), datetime.datetime.now(), data, "")
     blockchain.add_block(new_block)
     return render_template('index.html', chain=blockchain.chain)
 
